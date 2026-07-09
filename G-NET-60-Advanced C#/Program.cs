65d1cdb4db73ad6bed6f166ebdab05793ac08d0d
Abdelrahman Elhalby <abdoelhalby60@gmail.com>
2026-07-09 22:11:10 +0300

    Question01

--- G-NET-60-Advanced C#/Program.cs
+++ G-NET-60-Advanced C#/Program.cs
@@ -4,8 +4,24 @@ internal class Program
 {
     static void Main(string[] args)
     {
-       
 
+        #region Question01
+        //Q1: What is a generic class? Why use generics?
+
+
+        // A generic class is a class that works with different data types
+        // by using a type parameter (such as <T>) instead of a fixed type.
+
+        // Why use generics?
+        // 1. Code Reusability: Write one class that works with many data types.
+        // 2. Type Safety: Detect type errors at compile time.
+        // 3. Better Performance: Avoid boxing and unboxing with value types.
+        // 4. Cleaner Code: No need for casting when retrieving data.
+
+
+
+
+        #endregion
 
     }
 }
