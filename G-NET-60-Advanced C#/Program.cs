f518e2e409ac2681f732cde45632aedec4b7a860
Abdelrahman Elhalby <abdoelhalby60@gmail.com>
2026-07-10 01:15:51 +0300

    Question15

--- G-NET-60-Advanced C#/Program.cs
+++ G-NET-60-Advanced C#/Program.cs
@@ -228,12 +228,33 @@ internal class Program
         // For value types: 0, false, '\0', etc.
         // For reference types: null.
 
-            //public static T GetDefault<T>()
-            //    {
-            //        return default;
-            //    }
+        //public static T GetDefault<T>()
+        //    {
+        //        return default;
+        //    }
 
         #endregion
 
-}
+        #region Question14
+
+        //Q14: Write a SafeList<T> that returns default when the index is invalid.
+
+        //public class SafeList<T>
+        //{
+        //    private List<T> items = new List<T>();
+        //    public void Add(T item) => items.Add(item);
+        //    public T Get(int index)
+        //    {
+        //        if (index < 0 || index >= items.Count)
+        //        {
+        //            return default;
+        //        }
+        //        return items[index];
+        //    }
+        //}
+
+        #endregion
+
+
+    }
 }
---
+++ G-NET-60-Advanced C#/SafeList.cs
@@ -0,0 +1,22 @@
+﻿
+namespace G_NET_60_Advanced_C_
+{
+    internal class SafeList<T>
+    {
+        private List<T> items = new List<T>();
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+
+        public T Get(int index)
+        {
+            if (index < 0 || index >= items.Count)
+            {
+                return default;
+            }
+            return items[index];
+        }
+    }
+}
